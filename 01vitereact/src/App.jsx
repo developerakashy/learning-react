177b3b3edc0374a8d92be8434870002e39b8b490
developerakashy <developerakashy@gmail.com>
2024-06-19 21:06:01 +0530

    creating first reactapp

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Hello World | Akash Yadav</h1>
+      <h3>This application is developed using - npm create vite@latest</h3>
+      <h3>Application can be runned using - npm run dev</h3>
+      <h3>It also support build command which is used for production level working</h3>
+    </>
+  )
+}
+
+export default App
