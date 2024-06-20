249f8cee6589502675adafdefc64befd2f4d0c46
developerakashy <developerakashy@gmail.com>
2024-06-20 12:29:10 +0530

    React

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <>
+      <h1>Learning react Day1</h1>
+      <h1>Hello World | Akash Yadav</h1>
+      <h3>This application is developed using - npx create-react-app 01basicreact</h3>
+      <h3>Application can be runned using - npm run start or npm start</h3>
+      <h3>It also support build command which is used for production level working</h3>
+    </>
+  );
+}
+
+export default App;
